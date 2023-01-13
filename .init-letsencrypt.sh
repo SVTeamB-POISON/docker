0fef1c10e2f75192ab4582174e85f2268bcd8858
sangminlee98 <tkdals0975@naver.com>
2023-01-13 21:47:23 +0900

    fix : fix certbot init file

--- .init-letsencrypt.sh
+++ .init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(localhost poisonsvb.com)
+domains=(poisonsvb.com)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="tkdals09789@gmail.com" # Adding a valid address is strongly recommended
