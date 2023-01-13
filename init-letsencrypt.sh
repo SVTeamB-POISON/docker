14df4c4ed2a64b5456423bd3cdc2477786903832
sangminlee98 <tkdals0975@naver.com>
2023-01-13 23:55:33 +0900

    chore : certbot 재설정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(poisonsvb.com)
+domains="poisonsvb.com"
 rsa_key_size=4096
-data_path="./nginx/certbot"
+data_path="./certbot"
 email="tkdals09789@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
